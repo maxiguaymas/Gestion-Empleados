eed76df5e5f637351da1be670b2c2f2dc7d31186
Eurnest <jasvier.ast@gmail.com>
2025-06-11 21:12:50 -0300

    Crear modelo de horarios de alto refinamiento

--- empleados/models.py
+++ empleados/models.py
@@ -22,6 +22,8 @@ class Empleado(models.Model):
     ruta_foto = models.ImageField(upload_to='empleados/fotos/', blank=True, null=True)
     fecha_ingreso = models.DateField(auto_now_add=True)
     fecha_egreso = models.DateField(blank=True, null=True)
+    
+
 
     def __str__(self):
         return f"{self.nombre} {self.apellido} - {self.dni}"
@@ -37,3 +39,15 @@ class Recibo_Sueldos(models.Model):
 
     def __str__(self):
         return f"Recibo {self.id_recibo} - {self.id_empl.nombre} {self.id_empl.apellido}"
+  #Clase horarios debe estar al mismo nivel que Empleado
+class Horarios(models.Model):
+    id_empl = models.ForeignKey('Empleado', on_delete=models.CASCADE, related_name='horarios')
+    turno = models.CharField(max_length=20, choices=[('Mañana', 'Mañana'), ('Tarde', 'Tarde')], default='Mañana')
+    dia = models.CharField(max_length=10, choices=[('Lunes', 'Lunes'), ('Martes', 'Martes'), ('Miércoles', 'Miércoles'), ('Jueves', 'Jueves'), ('Viernes', 'Viernes')])
+    hora_entrada = models.TimeField()
+    hora_salida = models.TimeField()
+    cantidad_personal = models.IntegerField(default=1)
+
+
+    def __str__(self):
+        return f"Horario {self.id_empl.nombre} {self.id_empl.apellido} - {self.dia}"
