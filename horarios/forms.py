26466f8d36d72ea1e80679293a3908fa59da97d7
Maximiliano Guaymas <maxikasta480@gmail.com>
2025-06-24 21:11:09 -0300

    Crear templates de cargar_horarios y asignar_horarios

--- horarios/forms.py
+++ horarios/forms.py
@@ -5,8 +5,21 @@ class HorarioForm(forms.ModelForm):
     class Meta:
         model = Horarios
         fields = ['turno', 'dia', 'hora_entrada', 'hora_salida', 'cantidad_personal']
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            if field.widget.__class__.__name__ == 'Select':
+                field.widget.attrs['class'] = 'form-select'
+            else:
+                field.widget.attrs['class'] = 'form-control'
         
 class AsignarHorarioForm(forms.ModelForm):
     class Meta:
         model = AsignacionHorario
-        fields = ['empleado', 'horario', 'estado']
+        fields = ['id_empl', 'id_horario', 'estado']
+        
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs['class'] = 'form-control'
--- horarios/views.py
+++ horarios/views.py
@@ -1,29 +1,60 @@
 from django.shortcuts import render, redirect
 from .forms import HorarioForm
-from .forms import AsignarHorarioForm
-from empleados.models import Horarios
+from empleados.models import Horarios,Empleado,AsignacionHorario
+from django.contrib import messages
 # Create your views here.
 def cargar_horario(request):
     if request.method == 'POST':
         form = HorarioForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('lista_horarios')
+            return redirect('asignar_horario')
     else:
         form = HorarioForm()
     return render(request, 'cargar_horario.html', {'form': form})
 
 def asignar_horario(request):
+    horarios = Horarios.objects.all()
+    empleados = Empleado.objects.all()
+
     if request.method == 'POST':
-        form = AsignarHorarioForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('lista_horarios')
-    else:
-        form = AsignarHorarioForm()
-    return render(request, 'asignar_horario.html', {'form': form})
+        asignaciones_realizadas = 0
+        for horario in horarios:
+            for i in range(1, horario.cantidad_personal + 1):
+                empleado_id = request.POST.get(f"empleado_{horario.id}_{i}")
+                if empleado_id:
+                    empleado = Empleado.objects.get(id=empleado_id)
+                    # Evita asignaciones duplicadas
+                    if not AsignacionHorario.objects.filter(id_empl=empleado, id_horario=horario).exists():
+                        AsignacionHorario.objects.create(
+                            id_empl=empleado,
+                            id_horario=horario,
+                            estado='Asignado'
+                        )
+                        asignaciones_realizadas += 1
+        if asignaciones_realizadas:
+            messages.success(request, f"{asignaciones_realizadas} asignaciones realizadas correctamente.")
+        else:
+            messages.info(request, "No se realizaron nuevas asignaciones.")
+        return redirect('asignar_horario')
+
+    return render(request, 'asignar_horario.html', {
+        'horarios': horarios,
+        'empleados': empleados,
+    })
 
 def ver_horarios_asig(request):
-    horarios = Horarios.objects.select_related('empleado').all()
-    return render(request, 'horarios/ver_horarios_asig.html', {'horarios': horarios})
+    horarios = []
+    dni = request.GET.get('dni')
+    if dni:
+        try:
+            empleado = Empleado.objects.get(dni=dni)
+            horarios = AsignacionHorario.objects.filter(id_empl=empleado).select_related('id_empl', 'id_horario')
+        except Empleado.DoesNotExist:
+            horarios = []
+
+    return render(request, 'ver_horarios_asig.html', {
+        'horarios': horarios,
+    })
+
 
